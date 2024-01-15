a066e3a0cdb03ad97aad3cbd1c526d89fb74964e
Danilo3236 <88911017+Danilo3236@users.noreply.github.com>
2024-01-15 14:11:10 -0300

    alterardata.php

---
+++ alterardata.php
@@ -0,0 +1,21 @@
+<?php //002cd
+if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+?>
+HR+cPt/nU5acHq6VJqljUdMPBjnD4yXnjybCOlCszgoVS1NvHQ+VTZe4r+TNpRbGVSBvr+V3Jmq2
+oipE5R7o4Fz41wEAozhpgyzSfG3pBpUWy8Eyff4e+9MD24bhRXRRNWeOZVELNTzjausu35qNGnsw
+G67fNAS3I2tTEejTFqImKRUn+4ZpNmjSSQzmJkr/mo2qKhLveKaGPzPTtqRXNKD7DIDyCxtz1aY9
+xRvMShKQw9mhBU/kvC/HWFZVKbT7lXr7C/v7Yk7u7O+mG2MGzGD0d1HJZSOYho1/0z2KoGrofNxB
+UgNbw78ehRL6+/CZNhKvh85OI4J/CuT/g3LLVDNQuMZGNnKSGfc4DUBYjARANcE/N7FwdhJjEhu6
+54gTzNInscfviqvJvcieZ+EeIigmV46cZVdl7/1nknEg/fofiDu7v+NtGMl4Pc/ZBuvFUDxtZIMN
+21GIPB73YcnHPey7imQVyh+yRstIu1KbU2nDsj/eRrJyABjmY+bnlfH+I5UlcTGYnj2elo5tiM1R
+PiDeO0UWgJqbCh++1GXDTKoGswPSsIJ3VNwdmXyledn/S+HRJNZTxBWohj8H8wGG8aDTsXkag6k6
+rnMX2LoSEtudipAQhQzgyZYgi7rI1cK6pT14QUyJgMKvm6/yB2/bG2BxX8Z5zMMnBl21cS4piCoY
+2ZYpSiImP+epZG8zDTYmsfB52Cpf9c7nzg/8CVh8QD0gi7ApqgqmoMLViCckR5nJ7kYpsV8ZcwNT
+h7RFA37iIzsGmMclERPElBjz/HomvG7Wk7TGJ8Cs8dXExqGzosfbRf0t/FAyHqtWz/EJfjS4Gu8V
+arGLc1ZmTuYXw/M8OULLX+pJ9KdJNOzcA3bY1lP2xRM5S1Gs7eCmLo+wnp7GcnzLFTw+w+ZM9/ko
+gZBnILQ1N8yrj0GClKMM2bBxWDs4ScCWVYZ4FGSAq0G94hMirKWQnVdC7pI08yVSWpIsZEDgnXDk
+/gsj8gUEO1aENpbLRLvezBIvYsyQy3jtiPrxZ0Ge9MW0bexRNstyCtmj+bHKXXyjGOjtC/fcps6b
+Zl4h9bqBfWvP8GJzg1yaui80LbabxubEcpUB1Ojqaoi+rd5SLbKUKdDbYjKQO8KYkVusjtuDGgzE
+Tnq9wB5X+w6w9BWiAFA2f/+DjKI0/lnUaluS2sg0sq44zCm5a8ZwxekEtUtio/b7vzr1AbD6GSMp
+t7kF43EpgNTtGGuOWw7JjzYh1QUeNFwr3R/2L842AvVGV3Nl0LwIW8jLZdmq/As4U11T2nKNTzPf
++6Q4yFMT+jIEfLA92K94sZMAyu+AR9swuLwe+RWSs9pKNmbqAcTuM2HZBGMV0BXEWzg5
